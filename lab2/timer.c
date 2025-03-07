292c6a38dd466e5e41e9038cd427bc217e0a9fd2
Rafael Rodrigues <rodrigues2005rafa@gmail.com>
2025-03-07 12:30:35 +0000

    get_cof

--- lab2/timer.c
+++ lab2/timer.c
@@ -32,16 +32,38 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (timer != 0 && timer != 1 && timer != 2) { // o timer recebe 0,1 ou 2 de acordo com a porta (unsigned de 8 bits = int)
+    return 1;
+  }
 
-  return 1;
+  uint8_t command = BIT(7) | BIT(6) | BIT(5); //ativar o bit 7 e 6 para ativar o read-back, ativo o BIT(5) não ativo o bit 4 pois queremos receber 
+                                            //apenas a configuração       <----- atenção que não sei se o bit 5 deve ser ativado
+
+  switch (timer)
+  {
+  case 0:
+    command |= BIT(1);
+    break;
+  
+  case 1:
+    command |= BIT(2);
+    break;
+
+  case 2:
+    command |= BIT(3);
+    break;
+  }
+
+  sys_outb(0x43, command); //vou mandar para a porta 43 (porta de control word) o comando que construi;
+
+  until_sys_inb(timer, *st); //vou receber a resposta do "timer" e guardar a consiguração em *st;
+
+  return 0;
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  //fazer o display conf que nada mais é que: de acordo com o "field" vou preencher cada um dos seus pontos
 
   return 1;
 }
