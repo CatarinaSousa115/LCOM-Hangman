598b59a2306b089e7bf310186ba6a0b959a2d109
Djeb01 <89352721+Djeb01@users.noreply.github.com>
2025-03-07 11:47:50 +0000

    timer get conf

--- lab2/timer.c
+++ lab2/timer.c
@@ -32,10 +32,24 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if(timer != 0x40 && timer != 0x41 && timer != 0x42) return 1; /*verificar se o valor do timer é válido*/
+  *st = BIT(7) | BIT(6) | BIT(5);
 
-  return 1;
+  switch (timer){  /*adicionar à configuração o bit do timer recebido*/
+    case 0x40:
+    *st |= BIT(1);
+    break;
+
+    case 0x41:
+    *st |= BIT(2);
+    break;
+
+    case 0x42:
+    *st |= BIT(3);
+    break;
+  }
+
+  return 0;
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
