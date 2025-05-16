2836c3ed0b195f6b8911603452ffba570b3ea3e6
matilde sousa <up202305502@up.pt>
2025-05-16 12:26:50 +0100

    assets folder

---
+++ proj/src/assets/pixmap.h
@@ -0,0 +1,201 @@
+#pragma once
+
+#include <stddef.h>
+#include "lcom/xpm.h"
+
+static xpm_row_t const man_0[] = {
+  "32 20 4",          // Width: 32, Height: 20, 4 colors
+  "  0",              // ' ' = transparent or background
+  "x 2",              // x = dark color
+  "o 14",             // o = light color
+  "+ 4",              // + = medium color
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "xxxxxxxxxxxxxxxxxxx            "
+};
+
+static xpm_row_t const man_1[] = {
+  "32 20 4",          // Width: 32, Height: 20, 4 colors
+  "  0",              // ' ' = transparent or background
+  "x 2",              // x = dark color
+  "o 14",             // o = light color
+  "+ 4",              // + = medium color
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx            +---+   ",
+  "        xxx            |o o|   ",
+  "        xxx            +---+   ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "xxxxxxxxxxxxxxxxxxx            "
+};
+
+static xpm_row_t const man_2[] = {
+  "32 20 4",          // Width: 32, Height: 20, 4 colors
+  "  0",              // ' ' = transparent or background
+  "x 2",              // x = dark color
+  "o 14",             // o = light color
+  "+ 4",              // + = medium color
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx            +---+   ",
+  "        xxx            |o o|   ",
+  "        xxx            +---+   ",
+  "        xxx              |     ",
+  "        xxx              |     ",
+  "        xxx              |     ",
+  "        xxx              |     ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "xxxxxxxxxxxxxxxxxxx            "
+};
+
+static xpm_row_t const man_3[] = {
+  "32 20 4",          // Width: 32, Height: 20, 4 colors
+  "  0",              // ' ' = transparent or background
+  "x 2",              // x = dark color
+  "o 14",             // o = light color
+  "+ 4",              // + = medium color
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx            +---+   ",
+  "        xxx            |o o|   ",
+  "        xxx            +---+   ",
+  "        xxx              |     ",
+  "        xxx             /|     ",
+  "        xxx            / |     ",
+  "        xxx           o  |     ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "xxxxxxxxxxxxxxxxxxx            "
+};
+
+static xpm_row_t const man_4[] = {
+  "32 20 4",          // Width: 32, Height: 20, 4 colors
+  "  0",              // ' ' = transparent or background
+  "x 2",              // x = dark color
+  "o 14",             // o = light color
+  "+ 4",              // + = medium color
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx            +---+   ",
+  "        xxx            |o o|   ",
+  "        xxx            +---+   ",
+  "        xxx              |     ",
+  "        xxx             /|\\   ",
+  "        xxx            / | \\  ",
+  "        xxx           o  |  o  ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "xxxxxxxxxxxxxxxxxxx            "
+};
+
+static xpm_row_t const man_5[] = {
+  "32 20 4",          // Width: 32, Height: 20, 4 colors
+  "  0",              // ' ' = transparent or background
+  "x 2",              // x = dark color
+  "o 14",             // o = light color
+  "+ 4",              // + = medium color
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx            +---+   ",
+  "        xxx            |o o|   ",
+  "        xxx            +---+   ",
+  "        xxx              |     ",
+  "        xxx             /|\\   ",
+  "        xxx            / | \\  ",
+  "        xxx           o  |  o  ",
+  "        xxx             /      ",
+  "        xxx            /       ",
+  "        xxx           /        ",
+  "        xxx          +         ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "xxxxxxxxxxxxxxxxxxx            "
+};
+
+ static xpm_row_t const final_man[] = {
+  "32 20 4",          // Width: 32, Height: 20, 4 colors
+  "  0",              // ' ' = transparent or background
+  "x 2",              // x = dark color
+  "o 14",             // o = light color
+  "+ 4",              // + = medium color
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxxxxxxxxxxxxxxxxxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx             xxx    ",
+  "        xxx            +---+   ",
+  "        xxx            |o o|   ",
+  "        xxx            +---+   ",
+  "        xxx              |     ",
+  "        xxx             /|\\   ",
+  "        xxx            / | \\  ",
+  "        xxx           o  |  o  ",
+  "        xxx             / \\   ",
+  "        xxx            /   \\  ",
+  "        xxx           /     \\ ",
+  "        xxx          +       + ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "        xxx                    ",
+  "xxxxxxxxxxxxxxxxxxx            "
+};
+
