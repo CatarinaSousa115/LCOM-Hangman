c2cd871e2acdf9d8dcfff1975895a0e4c568cf5a
matilde sousa <up202305502@up.pt>
2025-05-23 09:50:03 +0100

    Terminal Init

--- proj/src/main.c
+++ proj/src/main.c
@@ -2,20 +2,103 @@
 
 #include "game/hangman.h"
 #include "peripherals/graphics/VBE.h"
-#include "peripherals/i8254.h"
+#include "peripherals/graphics/graphics.h"
 #include "peripherals/i8042.h"
+#include "peripherals/i8254.h"
+#include "peripherals/keyboard/keyboard.h"
+#include "peripherals/mouse/mouse.h"
 
-int (main)(int argc, char *argv[]) {
+extern uint8_t packet_byte_index;
 
+bool gameRunning = true;
+
+int(main)(int argc, char *argv[]) {
   lcf_set_language("EN-US");
   lcf_trace_calls("/home/lcom/labs/proj/trace.txt");
   lcf_log_output("/home/lcom/labs/proj/output.txt");
-  if (lcf_start(argc, argv)) return 1;
+
+  if (lcf_start(argc, argv))
+    return 1;
 
   lcf_cleanup();
+  return 0;
+}
+
+int process_interrupts() {
+  int ipc_status;
+  message msg;
+  uint8_t irq_kb, irq_mouse, irq_timer;
+
+  // Subscribe to interrupts
+  if (timer_subscribe_int(&irq_timer))
+    return 1;
+  if (keyboard_subscribe_int(&irq_kb))
+    return 1;
+  if (mouse_subscribe_int(&irq_mouse))
+    return 1;
+
+  // Enable mouse data reporting
+  if (mouse_enable_data_reporting())
+    return 1;
+
+  while (gameRunning) {
+    if (driver_receive(ANY, &msg, &ipc_status) != 0) {
+      printf("driver_receive failed\n");
+      continue;
+    }
+
+    if (is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+          // Timer interrupt
+          if (msg.m_notify.interrupts & irq_timer) {
+            timer_int_handler();
+            // Add timer-related logic here
+          }
+          // Keyboard interrupt
+          if (msg.m_notify.interrupts & irq_kb) {
+            kbc_ih();
+            // Add keyboard-related logic here
+          }
+          // Mouse interrupt
+          if (msg.m_notify.interrupts & irq_mouse) {
+            mouse_ih();
+            buffer_mouse_bytes();
+            if (packet_byte_index == 3) {
+              process_mouse_bytes();
+              packet_byte_index = 0; // Reset for the next packet
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  // Unsubscribe from interrupts
+  if (mouse_unsubscribe_int())
+    return 1;
+  if (keyboard_unsubscribe_int())
+    return 1;
+  if (timer_unsubscribe_int())
+    return 1;
 
   return 0;
 }
 
+int(proj_main_loop)(int argc, char *argv[]) {
+  // Initialize graphics mode
+  if (vg_init_graphic(0x14C) != 0) {
+    printf("Failed to initialize graphics mode. Exiting...\n");
+    return 1;
+  }
 
+  // Process interrupts
+  if (process_interrupts())
+    return 1;
 
+  // Exit graphics mode
+  vg_exit();
+  return 0;
+}
