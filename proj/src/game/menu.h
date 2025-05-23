79c4f677a1591c5e29818fcad104d40be422ce63
matilde sousa <up202305502@up.pt>
2025-05-23 11:10:29 +0100

    Menu Init

---
+++ proj/src/game/menu.h
@@ -0,0 +1,13 @@
+#ifndef __MENU_H__
+#define __MENU_H__
+
+
+/*void draw_text(WINDOW *win, int y, int x, const char *text) {
+  if (win == NULL) {
+    win = stdscr; // Use the default screen if no window is provided
+  }
+  mvwprintw(win, y, x, "%s", text); // Print text at (y, x) in the window
+  wrefresh(win);                    // Refresh the window to display the changes
+}
+*/
+#endif
