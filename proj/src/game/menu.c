632138b951c43a8ce78414936ca4e2706eaed46f
CatarinaSousa115 <up202306419@edu.fe.up.pt>
2025-05-26 16:55:12 +0100

    draw options
    Co-authored-by: RodriguesS2 <RodriguesS2@users.noreply.github.com>
    Co-authored-by: Djeb01 <Djeb01@users.noreply.github.com>

--- proj/src/game/menu.c
+++ proj/src/game/menu.c
@@ -3,19 +3,39 @@
 #include "../peripherals/graphics/graphics.h"
 
 void menu_init() {
-  
 
-  if (vg_draw_rectangle(150,150,100,100,0xFFFFFF) != 0) {
-    printf("Failed to draw rectangle\n");
-  }
+  draw_options();
 
-   if (draw_string("Hello-world !", 180, 180, 0xF47685, 1) != 0) {
-    printf("Failed to draw letter 'A'.\n");
-  }
-
-  
   return;
 }
 
+void draw_options() {
+  int total_height = 3 * BUTTON_HEIGHT + 2 * BUTTON_SPACING;
+  int y = (480 - total_height) / 2;
 
+  // Draw "Start" button
+  if (vg_draw_rectangle(total_height, y, BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_COLOR) != 0) {
+    printf("Failed to draw Start button\n");
+  }
+  if (draw_string("Start", total_height + 70, y + 15, TEXT_COLOR, 3) != 0) {
+    printf("Failed to draw Start text\n");
+  }
 
+  // Draw "Settings" button
+  y += BUTTON_HEIGHT + BUTTON_SPACING;
+  if (vg_draw_rectangle(total_height, y, BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_COLOR) != 0) {
+    printf("Failed to draw Settings button\n");
+  }
+  if (draw_string("Settings", total_height + 55, y + 15, TEXT_COLOR, 3) != 0) {
+    printf("Failed to draw Settings text\n");
+  }
+
+  // Draw "Exit" button
+  y += BUTTON_HEIGHT + BUTTON_SPACING;
+  if (vg_draw_rectangle(total_height, y, BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_COLOR) != 0) {
+    printf("Failed to draw Exit button\n");
+  }
+  if (draw_string("Exit", total_height + 75, y + 15, TEXT_COLOR, 3) != 0) {
+    printf("Failed to draw Exit text\n");
+  }
+}
--- proj/src/game/menu.h
+++ proj/src/game/menu.h
@@ -1,14 +1,21 @@
-#ifndef __MENU_H__
-#define __MENU_H__
+#ifndef _MENU_H_
+#define _MENU_H_
+
+#define BUTTON_WIDTH 200
+#define BUTTON_HEIGHT 50
+#define BUTTON_COLOR 0xFF99FF
+#define TEXT_COLOR 0xFFFFFF
+#define BUTTON_SPACING 20
 
 typedef enum {
-    MENU,
-    MENU_PLAY,
-    MENU_INSTRUCTIONS,
-    MENU_EXIT
+  MENU,
+  MENU_PLAY,
+  MENU_INSTRUCTIONS,
+  MENU_EXIT
 } MenuOptions;
 
 void menu_init();
+void draw_options();
 
 #endif
 
